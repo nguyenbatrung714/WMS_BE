9deccb77c99743876c62addac981d7e7c35d99e1
nguyenbatrung714 <trungnguyenba714@gmail.com>
2024-10-04 15:30:56 +0700

    WMS_BE

---
+++ src/main/java/org/example/wms_be/WmsBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.wms_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmsBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WmsBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/wms_be/WmsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.wms_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WmsBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
