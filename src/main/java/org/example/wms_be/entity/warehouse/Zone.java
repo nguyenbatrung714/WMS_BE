d07e39bd6b29c423c9049cd78cbbfbe1373a41a8
nguyenbatrung714 <trungnguyenba714@gmail.com>
2024-10-05 14:40:12 +0700

    BaTrung_Commit01: Create object and converter.

---
+++ src/main/java/org/example/wms_be/entity/warehouse/Zone.java
@@ -0,0 +1,14 @@
+package org.example.wms_be.entity.warehouse;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Zone {
+    private Integer sysIdKhuVuc;
+    private String maKhuVuc;
+    private String tenKhuVuc;
+    private String moTa;
+    private String maKho;
+}
