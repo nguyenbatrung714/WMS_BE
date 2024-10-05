1d0f83cd53fe559efa23319e8ceb8cf25c273191
nguyenbatrung714 <trungnguyenba714@gmail.com>
2024-10-05 15:37:07 +0700

    BaTrung_Commit02: Mapping sql.

---
+++ src/main/java/org/example/wms_be/mapper/warehouse/ZoneDetailMapper.java
@@ -0,0 +1,21 @@
+package org.example.wms_be.mapper.warehouse;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.example.wms_be.entity.warehouse.ZoneDetail;
+
+import java.util.List;
+
+@Mapper
+public interface ZoneDetailMapper {
+    List<ZoneDetail> getAllZoneDetails();
+
+    int insertZoneDetail(ZoneDetail zoneDetail);
+
+    int updateZoneDetail(ZoneDetail zoneDetail);
+
+    int deleteZoneDetail(String maChiTietKhuVuc);
+
+    boolean checkZoneDetailExists(String maChiTietKhuVuc);
+
+    ZoneDetail getZoneDetailByMaChiTietKhuVuc(String maChiTietKhuVuc);
+}
