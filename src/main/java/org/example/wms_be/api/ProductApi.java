f81a243588898d28ee4b6e8534783833006d9787
Andrew Hoang <hoangaatrox23504@gmail.com>
2024-10-26 16:19:27 +0700

    HuyHoang_Commit01: Public access

--- src/main/java/org/example/wms_be/api/ProductApi.java
+++ src/main/java/org/example/wms_be/api/ProductApi.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/products")
 @AllArgsConstructor
+@CrossOrigin
 public class ProductApi {
     private final ProductService productService;
 
