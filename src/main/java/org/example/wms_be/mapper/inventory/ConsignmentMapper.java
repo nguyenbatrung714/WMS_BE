7b8d3f97ff19a5267ac64471a6e74c748f7c24ba
nguyenbatrung714 <trungnguyenba714@gmail.com>
2024-11-04 13:39:06 +0700

    BaTrung_Commit07: Create mapper Lo Hang.

--- src/main/java/org/example/wms_be/mapper/inventory/ConsignmentMapper.java
+++ src/main/java/org/example/wms_be/mapper/inventory/ConsignmentMapper.java
@@ -1,7 +1,9 @@
 package org.example.wms_be.mapper.inventory;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.example.wms_be.entity.inventory.Consignment;
 
 @Mapper
-public class ConsignmentMapper {
+public interface ConsignmentMapper {
+    int insertConsignment(Consignment consignment);
 }
