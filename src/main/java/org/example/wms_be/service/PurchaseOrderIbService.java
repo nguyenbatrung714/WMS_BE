e80756abc0d1151a84bbbbf88da6983eddcdcb71
nguyenbatrung714 <trungnguyenba714@gmail.com>
2024-11-05 09:49:44 +0700

    BaTrung_Commit05: Get all purchase order and details.

--- src/main/java/org/example/wms_be/service/PurchaseOrderIbService.java
+++ src/main/java/org/example/wms_be/service/PurchaseOrderIbService.java
@@ -3,7 +3,11 @@ package org.example.wms_be.service;
 import org.example.wms_be.data.request.PurchaseOrderIbReq;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 public interface PurchaseOrderIbService {
     @Transactional
     PurchaseOrderIbReq createPurchaseOrder(PurchaseOrderIbReq purchaseOrderIbReq);
+
+    List<PurchaseOrderIbReq> getAllPurchaseOrders();
 }
