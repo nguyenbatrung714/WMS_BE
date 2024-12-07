ff621f7c1c0b924bb10fca2e9bddff1b2b5ee111
tramy1511 <nguyentramy151124@gmail.com>
2024-12-07 21:11:55 +0700

    TraMy_Commit01:thống kê

--- src/main/java/org/example/wms_be/service/PurchaseDetailsObService.java
+++ src/main/java/org/example/wms_be/service/PurchaseDetailsObService.java
@@ -1,9 +1,15 @@
 package org.example.wms_be.service;
 
 import org.example.wms_be.data.response.PurchaseRequestDetailsObResp;
+import org.example.wms_be.data.response.inbound.PurchaseRequestIbResp;
 
 import java.util.List;
 
 public interface PurchaseDetailsObService {
     List<PurchaseRequestDetailsObResp> getChiTietDonHangByMaPR(String maPR);
+    //Thống kê sản phẩm nhập nhiều nhất
+    List<PurchaseRequestDetailsObResp> getMostObProducts();
+
+    // Thống kê sản phẩm nhập ít nhất
+    List<PurchaseRequestDetailsObResp> getLeastObProducts();
 }
